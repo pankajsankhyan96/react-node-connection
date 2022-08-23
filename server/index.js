cc9f728c21aa1f89eb99404b32a6a26bead07a4d
manish negi <manish.negi@dipoletechi.com>
2022-08-23 11:58:33 +0530

    first branch commmit

--- server/index.js
+++ server/index.js
@@ -5,16 +5,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "API called from react!" });
 });
 
 app.get('/', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-    res.json({ message: "Welcome to node server!" });
-    // res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    // res.json({ message: "Welcome to node server!" });
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
