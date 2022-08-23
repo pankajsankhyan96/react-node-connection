cb09e2a0f0e1a5f27c436bcadfe14ec2b41be8f9
manish negi <manish.negi@dipoletechi.com>
2022-08-23 14:10:12 +0530

    fd

--- client/src/App.js
+++ client/src/App.js
@@ -41,8 +41,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {process.env.DB_HOST}
-          {JSON.stringify(process.env)}
+          Learn React : {process.env.REACT_APP_ENV}
+          :  {JSON.stringify(process.env)}
         </a>
 
         <p>
